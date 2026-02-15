994050294e6f29d223956b1402c62ff47ae41b34
Rishabh <rishabhdubey1472004@gmail.com>
2026-02-15 23:55:25 +0530

    solved questions

--- src/main/java/CodingBatProblems/Array2.java
+++ src/main/java/CodingBatProblems/Array2.java
@@ -230,4 +230,19 @@ public class Array2 {
         return nums;
     }
 
+    public int[] zeroMax(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                int maxOdd = 0;
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[j] % 2 == 1 && nums[j] > maxOdd) {
+                        maxOdd = nums[j];
+                    }
+                }
+                nums[i] = maxOdd;
+            }
+        }
+        return nums;
+    }
 }
