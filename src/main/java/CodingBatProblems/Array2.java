42eb1b5d662d9c06ebb0245e9f0a1830065c3a36
Rishabh <rishabhdubey1472004@gmail.com>
2026-02-16 00:50:14 +0530

    Array2 completed

--- src/main/java/CodingBatProblems/Array2.java
+++ src/main/java/CodingBatProblems/Array2.java
@@ -415,5 +415,20 @@ public class Array2 {
         nums[nums.length - 1] = first;
         return nums;
     }
+    public boolean modThree(int[] nums) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            boolean allEven = nums[i] % 2 == 0 &&
+                    nums[i + 1] % 2 == 0 &&
+                    nums[i + 2] % 2 == 0;
 
+            boolean allOdd = nums[i] % 2 != 0 &&
+                    nums[i + 1] % 2 != 0 &&
+                    nums[i + 2] % 2 != 0;
+
+            if (allEven || allOdd) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
