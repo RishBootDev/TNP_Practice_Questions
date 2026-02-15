c76189061d63e8346a46045608a812ea9b0e162c
Rishabh <rishabhdubey1472004@gmail.com>
2026-02-16 01:41:45 +0530

    Arrays3 completed

--- src/main/java/CodingBatProblems/Array3.java
+++ src/main/java/CodingBatProblems/Array3.java
@@ -1,14 +1,14 @@
 package CodingBatProblems;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Array3 {
 
     public static void main(String[] args) {
         Array3 array3 = new Array3();
         array3.squareUp(4);
+
+        System.out.println(Arrays.toString(array3.seriesUp(4)));
     }
 
     public int maxSpan(int[] nums) {
@@ -50,10 +50,10 @@ public class Array3 {
     public boolean linearIn(int[] outer, int[] inner) {
 
         Set<Integer> set = new HashSet<>();
-        for(int x : outer) set.add(x);
+        for (int x : outer) set.add(x);
 
-        for(int x : inner){
-            if(!set.contains(x)) return false;
+        for (int x : inner) {
+            if (!set.contains(x)) return false;
 
         }
 
@@ -83,14 +83,121 @@ public class Array3 {
 
         Set<Integer> set = new HashSet<>();
         int count = 0;
-        for (int i = 0; i < nums.length-1; i++) {
-            if(!set.contains(nums[i]) && nums[i] == nums[i+1]){
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (!set.contains(nums[i]) && nums[i] == nums[i + 1]) {
                 count++;
                 set.add(nums[i]);
-            }else if(nums[i] !=nums[i+1]) set.remove(nums[i]);
+            } else if (nums[i] != nums[i + 1]) set.remove(nums[i]);
         }
 
         return count;
     }
+
+    public int maxMirror(int[] nums) {
+        int n = nums.length;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                int left = i;
+                int right = j;
+                int count = 0;
+
+                while (left < n && right >= 0 && nums[left] == nums[right]) {
+                    count++;
+                    left++;
+                    right--;
+                }
+
+                max = Math.max(max, count);
+            }
+        }
+        return max;
+    }
+
+    public int[] fix45(int[] nums) {
+
+        if (nums.length == 0) return nums;
+
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            if (nums[i] == 4 && nums[i + 1] != 5) {
+                list1.add(i + 1);
+            }
+
+            if (nums[i] == 5 && (i == 0 || nums[i - 1] != 4)) {
+                list2.add(i);
+            }
+        }
+
+        int last = nums.length - 1;
+        if (nums[last] == 5 && nums[last - 1] != 4) {
+            list2.add(last);
+        }
+        int h = 0;
+        for (int i = 0; i < list1.size(); i++) {
+            int temp = nums[list1.get(i)];
+            nums[list1.get(i)] = nums[list2.get(h)];
+            nums[list2.get(h)] = temp;
+            h++;
+        }
+
+        return nums;
+    }
+
+    public int[] fix34(int[] nums) {
+
+        if (nums.length == 0) return nums;
+
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            if (nums[i] == 3 && nums[i + 1] != 4) {
+                list1.add(i + 1);
+            }
+
+            if (nums[i] == 4 && (i == 0 || nums[i - 1] != 3)) {
+                list2.add(i);
+            }
+        }
+
+        int last = nums.length - 1;
+        if (nums[last] == 4 && nums[last - 1] != 3) {
+            list2.add(last);
+        }
+
+        int h = 0;
+        for (int i = 0; i < list1.size(); i++) {
+            int temp = nums[list1.get(i)];
+            nums[list1.get(i)] = nums[list2.get(h)];
+            nums[list2.get(h)] = temp;
+            h++;
+        }
+
+        return nums;
+    }
+
+    public int[] seriesUp(int n) {
+
+        int temp = 1;
+        int res[] = new int[n*(n+1)/2];
+
+        for (int i = 0; i < res.length; ) {
+            int k = 1;
+            for (int j = 0; j < temp; j++) {
+                res[i] = k++;
+                i++;
+            }
+            temp++;
+        }
+        return res;
+    }
+
 
 }
