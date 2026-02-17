c117489c5058454a85aca7ee20bdeda04ad7e727
Rishabh <rishabhdubey1472004@gmail.com>
2026-02-17 13:34:27 +0530

    Arrays3 completed

---
+++ src/main/java/CodingBatProblems/Outer.java
@@ -0,0 +1,58 @@
+package CodingBatProblems;
+
+public class Outer {
+
+
+    // this is a important concept of the member inner class
+    void display() {
+        class Temp{
+            void show(){
+                System.out.println("this is a class inside a method");
+            }
+        }
+
+        Temp temp = new Temp();
+        temp.show();
+    }
+
+    class Inner{
+        public void show(){
+            System.out.println("hello");
+        }
+    }
+
+    public void test() {
+        Outer.Inner inner= new Inner();
+        inner.show();
+    }
+
+    public static void main(String[] args) {
+//        Outer outer = new Outer();
+//        outer.test();
+
+//        Outer.Inner inner = new Outer().new Inner();
+//        inner.show();
+
+        Outer outer = new Outer();
+        outer.display();
+
+    }
+}
+
+class James{
+
+    String name = "james";
+
+    class Inner{
+        String name = "peter";
+        void show() {
+            System.out.println(James.this.name);
+            System.out.println(this.name);
+        }
+    }
+
+    public static void main(String[] args) {
+        James.Inner inner = new James().new Inner();
+        inner.show();
+    }
+}
