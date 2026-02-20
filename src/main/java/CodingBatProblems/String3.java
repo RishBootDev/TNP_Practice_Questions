6bd55ac2cec81316c95a098f2500c4c765373969
Rishabh <rishabhdubey1472004@gmail.com>
2026-02-20 21:38:55 +0530

    solved a question

--- src/main/java/CodingBatProblems/String3.java
+++ src/main/java/CodingBatProblems/String3.java
@@ -35,4 +35,18 @@ public class String3 {
         }
         return result.toString();
     }
+    public boolean equalIsNot(String str) {
+        int isCount = 0;
+        int notCount = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i <= str.length() - 2 && str.substring(i, i + 2).equals("is")) {
+                isCount++;
+            }
+            if (i <= str.length() - 3 && str.substring(i, i + 3).equals("not")) {
+                notCount++;
+            }
+        }
+        return isCount == notCount;
+    }
 }
